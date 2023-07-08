2650a4794351174745ea42692ceada7ab263d1f7
Smriti <smritimakkar173@gmail.com>
2023-07-09 00:26:32 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const Person = () => {
+    return (
+      <>
+        <h1> Name : Smriti</h1>
+        <h2> Last Name : Makkar</h2>
+        <h2> Age : 18 </h2>
+      </>
+    )
+}
+
+
+const App = () => {
+  const name='smriti';
+  const First = 'ganeshji';
+  const isNameShowing = true;
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert('Reload');
+    // counter=100; never modify the state means(useState) manually in react 
+    alert("you have changed counter" + counter)
+  } ,[counter]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>om ganeshay namah!!</h1>
+      <h1>hello, {First}</h1>
+      <p>hello, { isNameShowing ? name : 'someone'}</p>
+      <p> calculated number is : {2+2}</p> 
+
+      <div className="sec2">
+      <Person/>
+      </div>
+
+      <div class="learn">  { /* basic hooks and additional hooks are there in react which we can use.use of event in react onclick property in react*/ }
+        <button onClick={() => setCounter((prevCount) => (prevCount)-1)}>-</button>
+        <h1>{counter}</h1>
+        <button onClick={() => setCounter((prevCount) => (prevCount)+1)}>+</button>
+      </div>
     </div>
   );
 }
